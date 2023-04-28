6e8f45e211d10f897f21810c85176af0d51e1047
tsuzukihashi <tsudukihashi0817@gmail.com>
2023-04-29 01:03:00 +0900

    fix: Build

--- Sources/TsuzuKit/PressButtonStyle.swift
+++ Sources/TsuzuKit/PressButtonStyle.swift
@@ -1,14 +1,14 @@
 import SwiftUI
 
-struct PressButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
+public struct PressButtonStyle: ButtonStyle {
+    public func makeBody(configuration: Configuration) -> some View {
         return configuration.label
             .scaleEffect(configuration.isPressed ? 1.1 : 1)
             .animation(.spring(response: 0.2, dampingFraction: 0.9), value: configuration.isPressed)
     }
 }
 
-extension ButtonStyle where Self == PressButtonStyle {
+public extension ButtonStyle where Self == PressButtonStyle {
     static var squemo: Self {
         return .init()
     }
