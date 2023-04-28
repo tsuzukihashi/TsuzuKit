f01138511eb65b73bc3731ef0a38885bd2231917
tsuzukihashi <tsudukihashi0817@gmail.com>
2023-04-29 01:12:29 +0900

    add: CapsuleButtonStyle

---
+++ Sources/TsuzuKit/CapsuleButtonStyle.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+public struct CapsuleButtonStyle: ButtonStyle {
+    @Environment(\.colorScheme) var colorScheme
+
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .font(.body.bold())
+            .foregroundColor(colorScheme == .dark ? .white : .black)
+            .background(
+                Capsule()
+                    .fill(colorScheme == .dark ? .black : .white)
+            )
+    }
+}
+
+public extension ButtonStyle where Self == CapsuleButtonStyle {
+    static var capsule: Self {
+        return .init()
+    }
+}
