d898892b7367d6d27babf18d95faa0bf279bd80d
tsuzukihashi <tsudukihashi0817@gmail.com>
2023-06-29 21:33:17 +0900

    add: public

--- Sources/TsuzuKit/Utils/HapticFeedback.swift
+++ Sources/TsuzuKit/Utils/HapticFeedback.swift
@@ -3,19 +3,19 @@ import CoreHaptics
 
 #if os(iOS)
 public enum HapticFeedback {
-  static func notice(type: UINotificationFeedbackGenerator.FeedbackType) {
+  public static func notice(type: UINotificationFeedbackGenerator.FeedbackType) {
     let generator = UINotificationFeedbackGenerator()
     generator.prepare()
     generator.notificationOccurred(type)
   }
 
-  static func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+  public static func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
     let generator = UIImpactFeedbackGenerator(style: style)
     generator.prepare()
     generator.impactOccurred()
   }
 
-  static func selection() {
+  public static func selection() {
     let generator = UISelectionFeedbackGenerator()
     generator.prepare()
     generator.selectionChanged()
