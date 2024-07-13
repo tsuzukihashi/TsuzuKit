3616fdb1399b4291c8bdc6eb1869a0215857ffac
Ryo Tsuzukihashi <tsudukihashi0817@gmail.com>
2024-07-13 12:36:53 +0900

    Create ViewExtension.swift

---
+++ Sources/TsuzuKit/Extension/ViewExtension.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+extension View {
+  @ViewBuilder
+  func `if`<Content: View>(
+    _ condition: Bool,
+    @ViewBuilder transform: (Self) -> Content
+  ) -> some View {
+    if condition {
+      transform(self)
+    } else {
+      self
+    }
+  }
+
+  @ViewBuilder
+  func `if`<TrueContent: View, FalseContent: View>(
+    _ condition: Bool,
+    @ViewBuilder _ then: (Self) -> TrueContent,
+    @ViewBuilder `else`: (Self) -> FalseContent
+  ) -> some View {
+    if condition {
+      then(self)
+    } else {
+      `else`(self)
+    }
+  }
+}
